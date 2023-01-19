7e11dfdf4cca5928c765bd1d8ec9cfc0d5de3646
yulian5kov <yulian5kov@gmail.com>
2023-01-19 22:37:11 +0200

    Login Fragment added

--- app/src/main/java/com/example/app_comp/LoginActivity.kt
+++ app/src/main/java/com/example/app_comp/LoginActivity.kt
@@ -2,10 +2,23 @@ package com.example.app_comp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.app_comp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        //setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+    private fun init(){
+
+    }
+
 }
---
+++ app/src/main/java/com/example/app_comp/LoginFragment.kt
@@ -0,0 +1,46 @@
+package com.example.app_comp
+
+import android.os.Bundle
+import android.util.Patterns
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.app_comp.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    private fun validateInputData(): Boolean {
+
+        var email = binding.etEmail.text.toString()
+
+        if (binding.etEmail.text.toString().isEmpty()) {
+            binding.tiEmail.error = "Email Required"
+            binding.etEmail.requestFocus()
+            return false
+        } else if (binding.etPassword.text.toString().isEmpty()) {
+            binding.tiPassword.error = "Password Required"
+            binding.etPassword.requestFocus()
+            return false
+        } else if (email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            //email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+            binding.tiEmail.error = "Please enter a valid email"
+            binding.etEmail.requestFocus()
+            return false
+        }
+        return true
+    }
+
+}
