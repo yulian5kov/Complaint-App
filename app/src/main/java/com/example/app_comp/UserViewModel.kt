750ac9010c0199fec620e3344b55be6caf5574da
yulian5kov <yulian5kov@gmail.com>
2023-02-02 19:58:34 +0200

    restart from the UserActivity only

--- app/src/main/java/com/example/app_comp/UserViewModel.kt
+++ app/src/main/java/com/example/app_comp/UserViewModel.kt
@@ -7,7 +7,4 @@ class UserViewModel : ViewModel()  {
 
     private val repo: FirestoreRepository = FirestoreRepository()
 
-    fun postComplaint(complaint: String): Flow<Result<Unit>> = repo.postComplaint(complaint)
-    fun getComplaints(userId: String): Flow<Result<List<Complaint>>>  = repo.getComplaints(userId)
-    //userId: String - userId
 }
