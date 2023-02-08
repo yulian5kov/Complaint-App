8a0da7a4ced973f7f7e69faddcfa474f0eef2799
yulian5kov <yulian5kov@gmail.com>
2023-02-08 17:47:51 +0200

    attemped changes to fix the PCF situation

--- app/src/main/java/com/example/app_comp/PostComplaintFragment.kt
+++ app/src/main/java/com/example/app_comp/PostComplaintFragment.kt
@@ -1,16 +1,20 @@
 package com.example.app_comp
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+//import androidx.fragment.app.OnBackPressedCallback
 import androidx.activity.addCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.app_comp.databinding.FragmentPostComplaintBinding
-import androidx.navigation.fragment.findNavController
+import kotlin.system.exitProcess
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentActivity
 
 
 class PostComplaintFragment : Fragment() {
@@ -18,11 +22,13 @@ class PostComplaintFragment : Fragment() {
     private val viewModel: UserViewModel by viewModels()
     private lateinit var backCallback: OnBackPressedCallback
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentPostComplaintBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -37,12 +43,61 @@ class PostComplaintFragment : Fragment() {
             val complaint = Complaint(complaintText)
             viewModel.postComplaint(complaint)
         }
+
+        backCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Log.d(DEBUGGING, "mazalqk")
+                requireActivity().onBackPressed()
+            }
+        }
+        //requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, backCallback)
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
-            val intent = Intent(requireContext(), UserActivity::class.java)
-            startActivity(intent)
-            requireActivity().finish()
+            requireActivity().supportFragmentManager.popBackStack()
         }
 
+    }
+
+    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
+
+        if (item.itemId == android.R.id.home) {
+            requireFragmentManager().popBackStack()
+            //activity?.onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //(activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            //requireActivity().onBackPressed()
+            activity?.onBackPressed()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        //backCallback.remove()
+        requireActivity().onBackPressedDispatcher.removeCallback(viewLifecycleOwner)
+    }
+
+    fun returnToUserActivity() {
+//        supportFragmentManager.findFragmentByTag(PostComplaintFragment.TAG)?.let { fragment ->
+//            supportFragmentManager.beginTransaction().remove(fragment).commit()
+//        }
+        fragmentManager?.let { fm ->
+            fm.findFragmentByTag(POST_COMPLAINT_FRAGMENT)?.let { fragment ->
+                fm.beginTransaction().remove(fragment).commit()
+            }
+        }
+        //finish()
+        activity?.finish()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        requireActivity().onBackPressedDispatcher.removeCallback(backCallback)
     }
 
 }
